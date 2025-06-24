a8f2c470948e7ee2602d4e4dac9da807027d5d19
ZakirTqvi <husainitaqvi4@gmail.com>
2025-06-24 22:01:45 +0530

    New files are added in Day1-pattern folder

--- Day1-patterns/program2.cpp
+++ Day1-patterns/program2.cpp
@@ -1,15 +1,21 @@
+/* Program to print pattern usinf characters*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+
     int n = 4;
+
     for (int i = 0; i < n; i++)
     {
+        char ch = 'A';
         for (int j = 0; j < n; j++)
         {
-            cout << j + 1 << " ";
+            cout << ch << " ";
+            ch = ch + 1;
         }
 
         cout << endl;
--- Day1-patterns/program3.cpp
+++ Day1-patterns/program3.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
+
     int n = 4;
+    char ch = 'A';
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << j + 1 << " ";
+            cout << ch << " ";
+            ch = ch + 1;
         }
 
         cout << endl;
